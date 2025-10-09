c27be09b840011be9b2b42afd5f8c6a190a9e39d
Noah_lindquist <2029lindn69@moundsviewschools.org>
2025-10-09 13:46:52 +0000

    deleted:    export_presets.cfg 	deleted:    exports/web/godot_web_game.apple-touch-icon.png 	deleted:    exports/web/godot_web_game.apple-touch-icon.png.import 	deleted:    exports/web/godot_web_game.audio.position.worklet.js 	deleted:    exports/web/godot_web_game.audio.worklet.js 	deleted:    exports/web/godot_web_game.html 	deleted:    exports/web/godot_web_game.icon.png 	deleted:    exports/web/godot_web_game.icon.png.import 	deleted:    exports/web/godot_web_game.js 	deleted:    exports/web/godot_web_game.pck 	deleted:    exports/web/godot_web_game.png 	deleted:    exports/web/godot_web_game.png.import 	deleted:    exports/web/godot_web_game.wasm 	deleted:    gamestate.gd 	deleted:    gamestate.gd.uid 	deleted:    global.gd 	deleted:    global.gd.uid 	deleted:    players/player1.gd 	deleted:    players/player1.gd.uid 	deleted:    players/player1.tscn 	deleted:    players/player2.gd 	deleted:    players/player2.gd.uid 	deleted:    players/player2.tscn 	deleted:    players/player_1Soccer.tscn 	deleted:    players/player_2Soccer.tscn 	deleted:    project.godot 	deleted:    scenes/clicker.gd 	deleted:    scenes/clicker.gd.uid 	deleted:    scenes/clicker.tscn 	deleted:    scenes/levels/level1.tscn 	deleted:    scenes/levels/level2.tscn 	deleted:    scenes/levels/level3.tscn 	deleted:    scenes/levels/level4.tscn 	deleted:    scenes/levels/level5.tscn 	deleted:    scenes/levels/level6.tscn 	deleted:    scenes/levels/level7.tscn 	deleted:    scenes/main_menu.gd 	deleted:    scenes/main_menu.gd.uid 	deleted:    scenes/main_menu.tscn 	deleted:    scenes/soccer.gd 	deleted:    scenes/soccer.gd.uid 	deleted:    scenes/soccer.tscn 	deleted:    scenes/test.gd 	deleted:    scenes/test.gd.uid 	deleted:    scenes/test.tscn 	deleted:    sprites/Cracked_tile_set.png 	deleted:    sprites/Cracked_tile_set.png.import 	deleted:    sprites/Dark_button.png 	deleted:    sprites/Dark_button.png.import 	deleted:    sprites/Light_button.png 	deleted:    sprites/Light_button.png.import 	deleted:    sprites/OneWay.png 	deleted:    sprites/OneWay.png.import 	deleted:    sprites/Tile_set.png 	deleted:    sprites/Tile_set.png.import 	deleted:    sprites/background.png 	deleted:    sprites/background.png.import 	deleted:    sprites/background_complete.png 	deleted:    sprites/background_complete.png.import 	deleted:    sprites/barrier_dark.png 	deleted:    sprites/barrier_dark.png.import 	deleted:    sprites/barrier_light.png 	deleted:    sprites/barrier_light.png.import 	deleted:    sprites/key.png 	deleted:    sprites/key.png.import 	deleted:    sprites/level_end.png 	deleted:    sprites/level_end.png.import 	deleted:    sprites/player1spritesheet.png 	deleted:    sprites/player1spritesheet.png.import 	deleted:    sprites/player1spritesheetboost.png 	deleted:    sprites/player1spritesheetboost.png.import 	deleted:    sprites/player2spritesheet.png 	deleted:    sprites/player2spritesheet.png.import 	deleted:    sprites/playercomplete.png 	deleted:    sprites/playercomplete.png.import 	deleted:    sprites/portal.png 	deleted:    sprites/portal.png.import 	deleted:    sprites/soccerball.png 	deleted:    sprites/soccerball.png.import 	deleted:    sprites/tiles.tres     deleted:    trash/icon.svg 	deleted:    trash/icon.svg.import 	deleted:    trash/player1.png 	deleted:    trash/player1.png.import 	deleted:    trash/player2.png 	deleted:    trash/player2.png.import 	deleted:    trash/test.png 	deleted:    trash/test.png.import 	deleted:    trash/trash.gd 	deleted:    trash/trash.gd.uid 	deleted:    trash/trash.png 	deleted:    trash/trash.png.import 	deleted:    trash/trash2.gd 	deleted:    trash/trash2.gd.uid 	deleted:    trash/trash2.png 	deleted:    trash/trash2.png.import 	deleted:    trash/trash5.png 	deleted:    trash/trash5.png.import

--- scenes/clicker.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-var score : int = 0
-
-func _on_click_button_down():
-	score += 1
-	$score.set_text(str(score))
--- trash/trash2.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _ready():
-	Global.tagger = $player1/Area2D
-
-func _process(delta: float):
-	$Label.set_text(str(Global.tagger))
